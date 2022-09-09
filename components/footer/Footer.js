02e9ad7faf35693db1635407d20418b23a4f3cd0
Igor Souza <soujigor@gmail.com>
2022-09-09 15:41:04 -0300

    Adicionando Footer

---
+++ components/footer/Footer.js
@@ -0,0 +1,75 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillBehanceSquare } from "react-icons/ai";
+import { FaInstagramSquare } from "react-icons/fa";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/dist/client/image";
+import soujiLogo from "../images/soujigorLogo.png";
+
+const footerList = [
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/luciana-ribeiro-designer-grafico/",
+    icon: <BsLinkedin size="26px" />,
+  },
+  {
+    name: "Behance",
+    href: "https://www.behance.net/lu_ribeiro",
+    icon: <AiFillBehanceSquare size="34px" />,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/lu.ri.beiro",
+    icon: <FaInstagramSquare size="30px" />,
+  },
+];
+
+const Footer = () => {
+  return (
+    <Flex justify="space-between" w="100%" paddingTop="10px">
+      <Flex
+        justify="center"
+        alignItems="center"
+        w="100px"
+        h="100px"
+        cursor="pointer"
+      >
+        <Flex as={motion.div} whileHover={{ scale: 1.1 }} cursor="pointer">
+          <Link href="https://www.linkedin.com/in/soujigor/">
+            <a target="_blank">
+              <Image src={soujiLogo} objectFit="" alt="logo" />
+            </a>
+          </Link>
+        </Flex>
+      </Flex>
+      <Flex gap="20px" justify="center" alignItems="center" cursor="pointer">
+        {footerList.map((footerItem) => {
+          return (
+            <Flex
+              h="100%"
+              w="100%"
+              justifyContent="center"
+              alignItems="center"
+              key={footerItem.name}
+              as={motion.div}
+              whileHover={{ scale: 1.1 }}
+              cursor="pointer"
+            >
+              {footerItem.icon ? (
+                <Link href={footerItem.href}>
+                  <a target="_blank">{footerItem.icon}</a>
+                </Link>
+              ) : (
+                <Link href={footerItem.href}></Link>
+              )}
+            </Flex>
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { Box, Flex } from "@chakra-ui/react";
 import Header from "../components/header/Header";
 import Main from "../components/main/Main";
-import Info from "../components/Info/Info";
+import Footer from "../components/footer/Footer";
+
 
 export default function Home() {
   return (
@@ -18,25 +17,9 @@ export default function Home() {
       <Box maxWidth="1300px" width="100%">
         <Header />
         <Main />
+        <Footer />
 
-        {/* <footer className={styles.footer}>
-          <a
-          <Info />
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Powered by{" "}
-          <span className={styles.logo}>
-          <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={72}
-          height={16}
-          />
-          </span>
-          </a>
-        </footer> */}
+       
       </Box>
     </Flex>
   );
