02f3278761084b15ef7207603a5dd025d8838d3f
Igor Ferraz <soujigor@gmil.com>
2023-03-13 21:45:17 -0300

    lala

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Box, Flex } from "@chakra-ui/react";
-import Header from "../components/header/Header";
+
 import Main from "../components/main/Main";
 import Footer from "../components/footer/Footer";
 
