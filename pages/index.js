5990c81bf9f54388373acdc656945e3e90d66ad3
Igor Ferraz <soujigor@gmil.com>
2023-03-13 21:45:35 -0300

    lala2

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,22 @@
 import Head from "next/head";
 import { Box, Flex } from "@chakra-ui/react";
-
 import Main from "../components/main/Main";
 import Footer from "../components/footer/Footer";
 
-
 export default function Home() {
   return (
-    <Flex >
+    <Flex>
       <Head>
         <title>Luciana Page</title>
-        <meta name="description" content="Portfólio de Designer. Design, Fotografia, Ux" />
+        <meta
+          name="description"
+          content="Portfólio de Designer. Design, Fotografia, Ux"
+        />
         <link rel="icon" href="/logo.png" />
-        
       </Head>
       <Box maxWidth="1300px" width="100%">
-        
         <Main />
         <Footer />
-
-       
       </Box>
     </Flex>
   );
